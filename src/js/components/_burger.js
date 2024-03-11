b1914e9e69dc040b72a2b7a20647912ee2bb03a0
valia <v.stohnii@elogic.com.ua>
2024-03-11 17:30:56 +0200

    fix footer

--- src/js/components/_burger.js
+++ src/js/components/_burger.js
@@ -1,13 +1,13 @@
-const navigation = document.querySelectorAll('.nav');
+const navigation = document.querySelector('.nav');
 const page = document.querySelector('.page');
 
-navigation.forEach(element => element.addEventListener('click', onBurgerBtnClick))
+navigation?.addEventListener('click', onBurgerBtnClick);
 
 function onBurgerBtnClick(e) {
   if (!e.target.parentNode.matches('.burger') && !e.target.matches('.burger')) {
     return;
   }
-console.log(this)
+
   this?.classList.remove('closed');
   page?.classList.add('open');
 
